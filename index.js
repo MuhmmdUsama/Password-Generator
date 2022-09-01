c6ed56b6a2209e0527fb0f8cc81e6c44c32888c9
Muhmmd-Usama <mummdusama@gmail.com>
2022-09-01 19:19:01 +0200

    Create a copy function

--- index.js
+++ index.js
@@ -29,35 +29,43 @@ function getSymbol() {
 function generatePassword() {
   let length = pwLengthEl.value;
   let passValue = '';
-
+  if (length >= '8' || length <= '20') {
     for (let i = 0; i < length; i++) {
       let x = generateCheck();
       passValue += x;
     }
+  } else {
+    alert('Your Password Length Must Between " 8 " and " 20 "');
+  }
 
   pwContaintEl.innerText = passValue;
-  console.log(passValue + ' length: '+ passValue.length)
+  console.log(passValue + ' length: ' + passValue.length);
 }
 
 function generateCheck() {
   let gChecked = [];
 
-  
   if (numberEl.checked) {
-      gChecked.push(getNumber());
-    }
-    if (lowerEl.checked) {
-        gChecked.push(getLower());
-    }
-    if (upperEl.checked) {
-        gChecked.push(getUpper());
-    }
-    if (symbolEl.checked) {
-        gChecked.push(getSymbol());
-    }
-    if (gChecked.length === 0) return '';
+    gChecked.push(getNumber());
+  }
+  if (lowerEl.checked) {
+    gChecked.push(getLower());
+  }
+  if (upperEl.checked) {
+    gChecked.push(getUpper());
+  }
+  if (symbolEl.checked) {
+    gChecked.push(getSymbol());
+  }
+  if (gChecked.length === 0) return '';
 
   return gChecked[Math.floor(Math.random() * gChecked.length)];
 }
 
 generateEl.addEventListener('click', generatePassword);
+
+copyEl.onclick = function () {
+  let content = pwContaintEl.textContent;
+  navigator.clipboard.writeText(content);
+  alert('Your Password Copied');
+};
